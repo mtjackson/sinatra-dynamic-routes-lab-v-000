8cbc55e037454c00eecaed17b22c6d514d92cb3f
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-15 21:25:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -31,7 +31,7 @@ class App < Sinatra::Base
     @num1 = params[:number1]
     @num2 = params[:number2]
     if params[:operation] == 'add'
-      "#{params[:]}"
+      "#{@num1.to_i + @num2.to_i}"
   end
 
 end

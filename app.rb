ebf3a01e379485bb4f449e8a25c37487f7a13ac5
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-15 21:27:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,8 +30,13 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @num1 = params[:number1]
     @num2 = params[:number2]
+
     if params[:operation] == 'add'
       "#{@num1.to_i + @num2.to_i}"
+    elsif params[:operation] == 'subtract'
+      "#{@num1.to_i - @num2.to_i}"
+    elsif params[:operation] == 'multiply'
+      "#{@num1.to_i * @num2.to_i}"
   end
 
 end
